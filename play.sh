9b7b68a45954fe09a1cb24b3b50ea98da562fc97
richardkaler <31756802+richardkaler@users.noreply.github.com>
2024-12-28 11:43:18 -0500

    Update play.sh
    Finally nailed this!

--- play.sh
+++ play.sh
@@ -1,96 +1,69 @@
 #!/bin/bash
 
-# Notify user that searching for files to play
+arg="$1"
+searchterm="$(echo "$1" | sed 's%\./%%')"
 
-if [[ "$1" == "-d" ]]; then
-    source playdir.sh
-    exit 0
+shortname="$(basename "$0")"
+
+if [[ -z "$arg" ]]; then
+    echo "Error - an argument is required"
+    echo "Ex) $shortname [media]"
+    exit 1
 fi
-#shellcheck disable=SC2001
-arg="$(echo "$1" |sed 's%\./%%')"
-
-rootaccess() { sudo -v > /dev/null 2>&1; }
-
-
-if ! which mpv >/dev/null; then
-    echo "mpv is not installed - attempting install now"
-    if rootaccess; then
-        sudo apt-get install mpv -y
-    else
-        echo "log in as a user with root access to install mpv"
-        echo "NOTE: $(whoami) does not have required root permissions"
-        exit 1
-    fi
-else
-    echo "mpv is already installed - proceeding with script"
-fi
-
-formats=(
-    "*flac"
-    "*.mp4"
-    "*.avi"
-    "*.mkv"
-    "*.mov"
-    "*.wmv"
-)
 
 summary() {
     echo "Plays files in the current directory"
     echo "Ex: play.sh [file]"
-    echo "NOTE: You may be asking for trouble if multiple files match your string"
+    echo "Tip: script plays one file at a time - limit your search"
     echo "File must have a valid extension: mpv, avi, mp4, wmv, flac, mov"
 }
 
-# Display summary if the only argument is "-h"
-if [[ "$1" == "-h" ]]; then
+if [[ "$1" =~ (h|-h|--h|-help|--help|help)$ ]]; then
     summary
     exit 0
-    #trigger the second conditional if there is no arg
-elif [[ -z "$1" ]]; then
-    for type in "${formats[@]}"
-    do
-        quant="$(find . -maxdepth 1 -type f -iname "$type" | wc -l)" #Prevents the opening of several simultaneous files which would jam  up a host
-        quant2="$(find . -maxdepth 1 -type l -iname "$type" | wc -l)" #Does the same thing but checks for symbolic links instead of just files
-        if [[ "$quant" -eq 1 ]] || [[ "$quant2" -eq 1 ]]; then
-            search="$(find . -iname "$type" | wc -l)"
-            if [[ "$search" -ne 0 ]]; then
-                echo attempting to play media now
-                screen -S mpvplay -dm find . -iname "$type" -exec mpv {} + &
-                echo media now detached in screen session
-            fi
-            exit
-        fi
-    done
+fi
+
+rootaccess() { sudo -v > /dev/null 2>&1; }
+
+mpvcheck="$(command -v mpv)"
+
+if [[ -z "$mpvcheck" ]]; then
+ echo "mpv is not installed - attempting install now"
+    if rootaccess; then
+        sudo apt-get install mpv -y
+    else
+        echo "log in as a user with root access to install mpv"
+        echo "NOTE: $USER has insufficient permissions"
+        exit 1
+    fi
+
+else
+    echo "mpv is already installed - proceeding with script"
+fi
+
+search() { find "$PWD" -maxdepth 1 -type f -iname "*$searchterm*" | grep -iE '\.avi|\.mp4|\.mkv|\.flac|\.wmv|\.mov'; }
+
+filecount="$(search | wc -l)"
+
+if [[ "$filecount" -gt 1 ]]; then
+    echo "Too many results - try narrowing your search"
+    exit 0
+elif [[ -z "$filecount" ]]; then
+    echo "No valid media found - check file integrity or narrow search"
+    exit 0
 fi
 
 
-## Check for matches and attempt to play media for the current directory
-#NOTE: this may seem redundant. However, last segment specifies no argument - and that's the difference
-for type in "${formats[@]}"; do
-    match=$(find .  -maxdepth 1 -type f -iname "$type" -iname "*$arg*" 2>/dev/null || \
-        find .  -maxdepth 1 -type f -iwholename "$type" -iname "*$arg*" 2>/dev/null)
-    if [[ -n "$match" ]]; then
-        echo "got a match"
-        echo "Playing file now. This may take a moment..."
-        echo initiating screen session to play media in background
-        sleep 1s &
-        if screen -S "$arg" -dm find  -maxdepth 1 -type f -iname "$type" -iname "*$arg*" -exec mpv {} 2>/dev/null +; then
-            wait
-        else
-            screen -S "$arg" -dm find -maxdepth 1 -type f -iwholename "$type" -wholename "*$arg*" -exec mpv {} 2>/dev/null +;
-        fi
-        exit 0
-    fi
-done
+screenplay() { screen -S mpvplay -dm find "$PWD" -maxdepth 1 -iname "*$searchterm*" -exec mpv {} +; }
 
-for type in "${formats[@]}"; do
-    findct=$(find "$PWD" -maxdepth 1 -type f -iname "$type" -iname "*$arg*"| wc -l)
-    if [[ "$findct" -ne 0 ]]; then
-        echo finished - exiting script
-        exit 0
-    fi
-done
+echo "Opening file with mpv now..."
 
-echo "No valid files found"
-echo "Try checking your file extension(s). Exiting script."
-exit 0
+if screenplay; then
+    sleep .4s
+    wait
+    exit 0
+else
+    echo "Error - failed to launch media"
+exit 1
+fi
+
